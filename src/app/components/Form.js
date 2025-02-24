1370314cf01d5346de1c0e0bf55b73f75145af3c
Muhammad Memon <ameenmemon2001@gmail.com>
2025-02-24 17:30:47 +0000

    complete lging page

--- src/app/components/Form.js
+++ src/app/components/Form.js
@@ -2,24 +2,71 @@ import * as React from "react";
 
 export default function Form() {
   return (
-    <div>
-      <h1>Welcome Back</h1>
-      <p>Welcome back! Please entr your details.</p>
-      <div>
+    <div className="bg-white px-10 py-20 rounded-3xl border-2 border-gray-200">
+      <h1 className="text-5xl font-semibold">Welcome Back</h1>
+      <p className="font-medium text-lg text-gray-500 mt-4">
+        Welcome back! Please enter your details.
+      </p>
+      <div className="mt-8">
         <div>
-          <label>Email</label>
-          <input className="" placeholder="Enter your email" />
+          <label className="text-lg font-medium">Email</label>
+          <input
+            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+            placeholder="Enter your email"
+          />
         </div>
         <div>
-          <label>Password</label>
-          <input className="" placeholder="Enter your password" />
+          <label className="text-lg font-medium">Password</label>
+          <input
+            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+            placeholder="Enter your password"
+          />
         </div>
 
-        <div>
+        <div className="mt-8 flex justify-between">
           <div>
             <input type="checkbox" id="remember" />
-            <label for="remember">Remember for 30 days</label>
+            <label className="ml-2 font-medium text-base" for="remember">
+              Remember for 30 days
+            </label>
           </div>
+          <button className="font-medium text-base text-violet-500">
+            Forgot password
+          </button>
+        </div>
+        <div className="mt-8 flex flex-col gap-y-4">
+          <button
+            className="active:scale-[.98] 
+                              active:duration-75 
+                              hover:scale-[1.03]
+                              transition-all
+                              ease-in-out
+                              py-3 
+                              rounded-xl 
+                              bg-violet-500
+                               text-white 
+                               text-lg 
+                               font-bold"
+          >
+            Sign in
+          </button>
+
+          <button
+            className="active:scale-[.98] rounded-xl
+                              active:duration-75 
+                              hover:scale-[1.03]
+                              
+                              transition-all gap-2 border border-gray-300 px-4 py-2 hover:bg-gray-100 flex items-center"
+          >
+            <img src="google.png" alt="Google Logo" className="w-8 h-8" />
+            <span className="text-gray-700">Sign in with Google</span>
+          </button>
+        </div>
+        <div className="mt-8 flex justify-center items-center">
+          <p className="font-medium text-base">Don't have an account?</p>
+          <button className="text-violet-500 text-base font-medium ml-2">
+            Sing up
+          </button>
         </div>
       </div>
     </div>
